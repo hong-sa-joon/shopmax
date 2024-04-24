f3694575bbc9741e83a681b254128ae696153495
hong-sa-joon <mek125@naver.com>
2024-04-24 17:36:23 +0900

    first commit

---
+++ shop.sql
@@ -0,0 +1,6 @@
+drop database shop;
+
+-- 한글 저장을 위해 UTF-8를 캐릭터 셋으로 하는 데이터베이스 생성
+CREATE DATABASE shop DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+use shop;
---
+++ src/main/java/com/shopmax/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.shopmax.repository;
+
+import com.shopmax.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
